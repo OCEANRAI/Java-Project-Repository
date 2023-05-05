960447d638165b337cf9914e3bef738dc90b50a0
W3schools-test <oceanrai2000@gmail.com>
2023-05-05 15:15:12 -0400

    This codes are all added while learning

---
+++ Main.java
@@ -0,0 +1,103 @@
+//public class MyProject {
+//  public static void main(String[] args) {
+//    System.out.println("Hello World");
+//}
+//}
+/*public class Main {
+    public static void main(String[] args){
+
+        System.out.println("Hello, my name is ocean, and I am learning java programming language");
+        System.out.println("I am so glad to learn java programming language");
+        System.out.println("Nice to meet you all");
+        System.out.print("this is print method without ln in the end. ");
+        System.out.print("this is also print method without ln in the end");
+    }
+}*/
+
+// This block of code is just for practice purposes only.
+// It is basically printing numbers, adding up and multipling it.
+/*public class Main {
+    public static void main(String[] args) {
+        System.out.println(255);
+        System.out.println(200);
+        System.out.println(500);
+        System.out.println(600);
+        System.out.println(7 * 7);
+        System.out.println(49 * 49);
+        System.out.println(25 + 49);
+
+    }
+}*/
+
+//Java Variables 
+/*public class Main {
+    public static void main(String[] args) {
+
+        String userName = "Ocean";
+        System.out.println(userName);
+        int myNum = 25;
+        myNum = 40;
+        //final int myNum = 50;
+        System.out.println(myNum);
+
+        
+    }
+}*/
+
+/*public class Main {
+    public static void main(String[] args) {
+        String firstName = "Ocean";
+        String lastName = "Rai";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);
+
+        // int x = 25;
+        // int y = 45;
+        // System.out.println(x + y);
+
+        // declaring many variables
+        // instead of writing this
+        // int a = 5;
+        // int b = 8;
+        // int c = 20;
+        // System.out.println(a + b - c);
+
+        // simply write
+        // int a = 5, b = 8, c = 20;
+        // System.out.println(a + b - c);
+
+        // one value to multiple variables
+        //int x, y, z;
+        //x = y = z = 20;
+        //System.out.println(x + y + z);
+
+    }
+
+}*/
+
+// Java Identifiers
+
+public class Main {
+    public static void main(String[] args) {
+        int minutesPerHour = 60;
+        System.out.println(minutesPerHour);
+
+        byte myNum = 100;
+        System.out.println(myNum);
+
+        short myNums = 5000;
+        System.out.println(myNums);
+
+        long mynumbs = 150000000000L;
+        System.out.println(mynumbs);
+
+        float mynumb = 5.75f;
+        System.out.println(mynumb);
+
+        double mynumber = 19.99d;
+        System.out.println(mynumber);
+
+
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # Java-Project-Repository
+
+
+Java Variables type
+
+String - stores text, such as "Hello". String values are surrounded by double quotes
+int - stores integers (whole numbers), without decimals, such as 123 or -123
+float - stores floating point numbers, with decimals, such as 19.99 or -19.99
+char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+boolean - stores values with two states: true or false
+
+
+int myNum = 5;
+float myFloatNum = 5.99f;
+char myLetter = 'D';
+boolean myBool = true;
+String myText = "Hello";
+
+
+
+Declaring (Creating) Variables
+
+type variableName = value;
+Where type is one of Java's types (such as int or String), and variableName is the name of the variable (such as x or name). The equal sign is used to assign values to the variable.
+
+
+
+Final Variables
+If you don't want others (or yourself) to overwrite existing values, use the final keyword (this will declare the variable as "final" or "constant", which means unchangeable and read-only):
+
+
+The general rules for naming variables are:
+
+Names can contain letters, digits, underscores, and dollar signs
+Names must begin with a letter
+Names should start with a lowercase letter and it cannot contain whitespace
+Names can also begin with $ and _ (but we will not use it in this tutorial)
+Names are case sensitive ("myVar" and "myvar" are different variables)
+Reserved words (like Java keywords, such as int or boolean) cannot be used as names
+
+Data types are divided into two groups:
+
+Primitive data types - includes byte, short, int, long, float, double, boolean and char
+Non-primitive data types - such as String, Arrays and Classes (you will learn more about these in a later chapter)
